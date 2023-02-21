d82bd588afaf52e3f0b27c9887ee0598ce95b64a
Aniket Kanere <aniket@pop-os.localdomain>
2023-02-21 14:22:00 +0530

    Fix package.json

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connect(CONNECTION_URL, {
 
 const connection = mongoose.connection;
 connection.on('error', console.error.bind(console, 'connection error: '));
-connection.once('open', function() {
+connection.once('open', function () {
   console.log('Connected successfully');
 });
 
